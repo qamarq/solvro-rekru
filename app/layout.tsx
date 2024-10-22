a828828404a68bf37246364f068a18751fc3344e
Kamil Marczak <kamilm@you2.pl>
2024-10-22 12:46:21 +0200

    footer updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,8 +72,8 @@ export default function RootLayout({
                         <Blobs />
                         <footer className='mt-10 border-t p-5'>
                             <div className='container mx-auto flex flex-col items-center justify-center'>
-                                <h1 className='font-cal text-lg'>Made with ❤️ by <Link href="https://kamilmarczak.pl">Kamil Marczak</Link> for Solvro</h1>
-                                <h3 className='text-muted-foreground text-sm'>© 2024 Kamil Marczak. All rights reserved.</h3>
+                                <h1 className='font-cal text-base'>Made with ❤️ by <Link href="https://kamilmarczak.pl" className='text-primary'>Kamil Marczak</Link> for <Link href="https://solvro.pwr.edu.pl/" className='text-primary'>Solvro</Link></h1>
+                                <h3 className='text-muted-foreground text-xs'>© 2024 Kamil Marczak. All rights reserved.</h3>
                             </div>
                         </footer>
                     </ReactQueryProvider>
